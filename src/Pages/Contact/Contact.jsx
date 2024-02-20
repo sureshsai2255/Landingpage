a27c819b76a35cbd74e6eade3bfe5485284772bd
Suresh kumar <sureshsai2255@gmail.com>
2024-02-20 16:45:58 +0530

    Updated address

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -4,8 +4,9 @@ export default function Contact() {
   return (
     <div>
       <h3>Contact</h3>
-      Country : India
-      Phone number  : 8143914575
+      Country : India <br />
+      Phone number  : 8143914575 <br />
+      Address : Kavali
     </div>
   )
 }
