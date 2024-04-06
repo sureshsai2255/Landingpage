67ca16f3449bb612054b144b9362d243f7d195c6
Suresh kumar <sureshsai2255@gmail.com>
2024-04-06 21:21:11 +0530

    updated font in contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,33 +2,33 @@ import './contact.css'
 
 export default function Contact() {
   return (
-    <div class="contact-wrapper">
-      <div class="content-right">
-        <div class="item address">
+    <div className="contact-wrapper">
+      <div className="content-right">
+        <div className="item address">
           <img src="/assets/icons8-address-64.png" alt="" />
           <p>Thumalavari street kavali, Nellore,Andhra Pradesh</p>
         </div>
-        <div class="item email">
+        <div className="item email">
         <img src="/assets/icons8-email-48.png" alt="" />
           <p>abc@example.com</p>
         </div>
-        <div class="item phone">
+        <div className="item phone">
         <img src="/assets/icons8-phone-48.png" alt="" />
           <p>number</p>
         </div>
       </div>
-      <div class="content-left">
+      <div className="content-left">
         <h2>Contact Me</h2>
 
-        <div class="form-wrapper">
-          <form class="form-fields">
-            <div class="name-wrapper"><input type="text" className="name" placeholder="Enter your name" /></div>
-            <div class="email-wrapper"><input type="email" className="email" placeholder="Enter your email" /></div>
-            <div class="message-wrapper"><textarea className="mssg" name="Message" placeholder='Message'></textarea></div>
+        <div className="form-wrapper">
+          <form className="form-fields">
+            <div className="name-wrapper"><input type="text" className="name" placeholder="Enter your name" /></div>
+            <div className="email-wrapper"><input type="email" className="email" placeholder="Enter your email" /></div>
+            <div className="message-wrapper"><textarea className="mssg" name="Message" placeholder='Message'></textarea></div>
           </form>
         </div>
-        <div class="buttons">
-          <button class="btn">Send</button>
+        <div className="buttons">
+          <button className="btn">Send</button>
         </div>
       </div>
     </div>
